cbae3123075463278bb2d8b804377d1461244a57
Shashank <136626472+shashank030718@users.noreply.github.com>
2025-01-24 14:34:08 +0530

    Add files via upload

---
+++ Image  color/textcolor.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+
+color = cv2.imread("butterfly.jpg",1)
+cv2.imshow("Image",color)
+cv2.moveWindow("Image",0,0)
+print(color.shape)
+height,width,channels = color.shape
+
+b,g,r = cv2.split(color)
+
+rgb_split = np.empty([height,width*3,3],'uint8')
+
+rgb_split[:,0:width] = cv2.merge([b,b,b])
+rgb_split[:,width:width*2] =  cv2.merge([g,g,g])
+rgb_split[:,width*2:width*3] = cv2.merge([r,r,r])
+
+cv2.imshow("Channels",rgb_split)
+cv2.moveWindow("Channels",0,height)
+
+cv2.waitKey(0)
